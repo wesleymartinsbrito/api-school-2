a0550fd0ffd867ccd875825036d1924e9135ef39
Wesley Martins <wesleymartins.dev@outlook.com>
2023-10-24 20:06:35 -0300

    DB and JPA dependecies, class Classe coming an table

--- src/main/java/com/school/demo/entities/Classe.java
+++ src/main/java/com/school/demo/entities/Classe.java
@@ -2,10 +2,20 @@ package com.school.demo.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_classe")
 public class Classe implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
