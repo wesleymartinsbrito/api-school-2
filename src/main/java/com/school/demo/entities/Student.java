eebc4cc868b06c5fff8eb32bc31a1b1e687aaefa
Wesley Martins <wesleymartins.dev@outlook.com>
2023-10-24 20:27:26 -0300

    Add foreing key of Classe in Student Class and ManyToOne Association

--- src/main/java/com/school/demo/entities/Student.java
+++ src/main/java/com/school/demo/entities/Student.java
@@ -1,11 +1,25 @@
 package com.school.demo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_student")
 public class Student {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Integer age;
 	
+	@ManyToOne
+	@JoinColumn(name = "class_id")
 	private Classe classe;
 	
 	public Student() {
