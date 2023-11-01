9b064790aec4399f12c6c61b39cb1b3a87f892a9
Wesley Martins <wesleymartins.dev@outlook.com>
2023-11-01 20:17:17 -0300

    DB configuration done

--- src/main/java/com/school/demo/controllers/StudentController.java
+++ src/main/java/com/school/demo/controllers/StudentController.java
@@ -3,6 +3,7 @@ package com.school.demo.controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +16,7 @@ import com.school.demo.entities.Student;
 public class StudentController {
 
 	@GetMapping
-	public List<Student> getObjects() {
-		Classe c1 = new Classe(1L, "Turma A");
-		Classe c2 = new Classe(2L, "Turma B");
-		
-		Student s1 = new Student(1L, "Maria", 15, c1);
-		Student s2 = new Student(2L, "João", 17, c2);
-		Student s3 = new Student(3L, "Felipe", 14, c1);
-		
-		List<Student> list = Arrays.asList(s1, s2, s3);
-		
-		return list;
+	public ResponseEntity getObjects() {
+		return ResponseEntity.ok("deu ok");
 	}
 }
